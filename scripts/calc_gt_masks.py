c66a7c78957c8d6f293f7b033139284d3edcc0e0
yumi <785618777@qq.com>
2020-09-22 15:56:50 +0800

    delete unused module (which causes problem on 16.04)

--- scripts/calc_gt_masks.py
+++ scripts/calc_gt_masks.py
@@ -18,7 +18,7 @@ from bop_toolkit_lib import visibility
 ################################################################################
 p = {
   # See dataset_params.py for options.
-  'dataset': 'lmo',
+  'dataset': 'custom-comb',
 
   # Dataset split. Options: 'train', 'val', 'test'.
   'dataset_split': 'test',
